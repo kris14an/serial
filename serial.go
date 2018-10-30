72a584419eb17c003ebc9bfb79e51f9dc54d5b8a
Krzysztof <kris14an@gmail.com>
2018-10-30 19:38:44 +0100

    Remove timeout limit for linux systems

--- serial.go
+++ serial.go
@@ -151,13 +151,6 @@ func posixTimeoutValues(readTimeout time.Duration) (vmin uint8, vtime uint8) {
 		// convert timeout to deciseconds as expected by VTIME
 		readTimeoutInDeci = (readTimeout.Nanoseconds() / 1e6 / 100)
 		// capping the timeout
-		if readTimeoutInDeci < 1 {
-			// min possible timeout 1 Deciseconds (0.1s)
-			readTimeoutInDeci = 1
-		} else if readTimeoutInDeci > MAXUINT8 {
-			// max possible timeout is 255 deciseconds (25.5s)
-			readTimeoutInDeci = MAXUINT8
-		}
 	}
 	return minBytesToRead, uint8(readTimeoutInDeci)
 }
